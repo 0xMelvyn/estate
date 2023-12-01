e436ed765c68050cd3050a3a51ccc4764b96979e
Melvyn <melvynhoarau@hotmail.fr>
2023-12-01 22:36:25 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,166 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client'
+import React, { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import { BsEnvelopeFill, BsInstagram, BsMessenger } from 'react-icons/bs';
+import design from '../public/design.png';
+import premier from '../public/premier.jpg';
+import deuxieme from '../public/deuxieme.jpg';
+import troisieme from '../public/troisieme.jpeg';
+import quatre from '../public/quatre.jpeg';
+import cinq from '../public/cinq.jpeg';
+import six from '../public/six.jpeg';
+import emailjs from 'emailjs-com';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
+  const notify = () => toast('Wow so easy!');
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_ok34pd9', 'template_yr9yz4q', form.current, 'pGbqVmTvTEyeeMMts')
+      .then(
+        (result) => {
+          toast.success('Email sent!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: darkMode ? 'dark' : 'light',
+          });
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const imageContainerB = document.querySelector('.image-container-b');
+      const imageContainerA = document.querySelector('.image-container-a');
+      const textSection = document.querySelector('#stop');
+      const textSectionPosition = textSection.offsetTop;
+
+      if (scrollPosition < textSectionPosition) {
+        imageContainerA.style.transform = `translateY(${scrollPosition / 2}px)`;
+        imageContainerB.style.transform = `translateY(-${scrollPosition / 2}px)`;
+      } else {
+        imageContainerA.style.transform = `translateY(${textSectionPosition / 2}px)`;
+        imageContainerB.style.transform = `translateY(-${textSectionPosition / 2}px)`;
+      }
+    };
+
+    const handleImageScroll = () => {
+      const scrollPosition = window.scrollY;
+      const imageContainerC = document.querySelector('.image-container-c');
+      const imageContainerD = document.querySelector('.image-container-d');
+      imageContainerC.style.transform = `translateY(-${scrollPosition / 10}px)`;
+      imageContainerD.style.transform = `translateY(${scrollPosition / 10}px)`;
+    };
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll);
+      window.addEventListener('scroll', handleImageScroll);
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('scroll', handleImageScroll);
+      }
+    };
+  },);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <ToastContainer />
+      <main className='bg-white px-8'>
+        <section className='min-h-screen'>
+          <nav className='pt-1 flex justify-between'>
+            <a href="#accueil"><Image src={design} width={250} height={250}/></a>
+            <ul className='flex items-center'>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="/">Accueil</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="/ventes">À vendre</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="/location">Location</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="#gestion">Gestion</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="#estimation">Estimation</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="#contact">Contact</a></h1></li>
+            </ul>
+          </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className='flex justify-around pt-14'>
+            <Image src={premier} width={250} height={250} className='w-1/6 h-1/6 pt-20 image-container-a' />
+            <Image id='stop' src={deuxieme} width={150} height={150} className='w-1/4'/>
+            <Image src={troisieme} width={250} height={250} className='w-1/6 h-1/6 pt-20 image-container-b'/>
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className='py-10' id="presentation">
+            <h3 className='text-5xl py-5 px-12'>SAVOIR FAIRE & SAVOIR VOIR</h3>
+              <div className='w-1/2 px-20 ml-auto'>
+                <p className='text-md py-2 leading-8 text-gray-800'>
+                L’expérience d’un accompagnement sur mesure, c’est identifier l’histoire racontée par le goût des clients : un accompagnement tel un confident de longue date.
+                <br />
+                La qualité du service et la responsabilité sont les conditions sine qua non de l’excellence de BORDAS immobilier.
+                <br />
+                L’achat de votre résidence, principale ou secondaire : qu’y a-t-il de plus beau dans une aventure comme celle-ci ? il n’y a aucun doute, le voyage est aussi important que la destination.
+                </p>
+                <p className='text-xl py-2 leading-8 text-black text-right mx-6 underline'>
+                  <a className='hover:text-gray-600' href="#ventes">Nos biens en vente</a>
+                </p>
+              </div>
+              <div className='flex justify-around pt-14'>
+                <Image src={quatre} className='w-1/4 h-1/4 image-container-c'/>
+                <Image src={cinq} className='w-1/4'/>
+                <Image src={six} className='w-1/4 h-1/4 pt-44 image-container-d'/>
+              </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <section className='py-10' id="services">
+              <div className='w-1/2 px-20'>
+                <p className='text-md py-2 leading-8 text-gray-800'>
+                L’écoute, le temps, la sincérité sont nos valeurs.
+                <br />
+                Passionnés d’architecture intérieur et design, nous saurons vous guider quant à la rénovation et la décoration de votre bien.
+                <br />
+                BORDAS, c’est aussi une rencontre avec des artistes contemporains, du Street art, des sculpteurs et des peintres venant de tous les continents car les univers de l’art et de l’immobilier se conjuguent autour d’un mot : l’esthétisme
+                </p>
+                <a href="#contact" className='text-xl py-2 leading-8 text-black underline hover:text-gray-600'>Nous contacter</a>
+              </div>
+        </section>
+        
+        <footer className='absolute left-0 bg-white text-center text-gray-700 w-full'>
+        <hr className="my-6 border-black" />
+          <div className='my-5'>
+            <a href="#accueil"><Image className='mx-auto' src={design} width={250} height={250}/></a>
+            <p className='text-black'>Horaires : </p>
+            <p>Du lundi au samedi de 9h00 à 19h00</p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+            <div className='flex justify-center text-2xl my-5 gap-5'>
+                <a className='hover:text-black transition duration-500' href="https://www.messenger.com/t/100088349492050/" target='_blank'><BsMessenger/></a>
+                <a className='hover:text-black transition duration-500' href="https://t.me/MelvynHoarau" target='_blank'><BsInstagram/></a>
+                <a className='hover:text-black transition duration-500' href="mailto:melvynhoarau@icloud.com" target='_blank'><BsEnvelopeFill/></a>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <p>contact@immobilier.com</p>
+            <p>07 67 85 68 37</p>
+            <div>
+                <small>&copy; MELVYN WEBDESIGN. All right reserved.</small>
+            </div>
+          </div>
+        </footer>
+      </main>
+    </div>
   )
 }
---
+++ pages/location.js
@@ -0,0 +1,131 @@
+import design from '../public/design.png';
+import app1 from '../public/app1.jpg';
+import app2 from '../public/app2.jpg';
+import app3 from '../public/app3.jpg';
+import app4 from '../public/app4.jpg';
+import Image from 'next/image';
+import { BsEnvelopeFill, BsInstagram, BsMessenger } from 'react-icons/bs';
+
+const ventes = () => {
+    return (
+      <div>
+        <main className='bg-white px-8'>
+        <section>
+          <nav className='pt-1 flex justify-between'>
+            <a href="#accueil"><Image src={design} width={250} height={250}/></a>
+            <ul className='flex items-center'>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="/">Accueil</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="/ventes">À vendre</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8 underline'><a href="/location">Location</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="#gestion">Gestion</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="#estimation">Estimation</a></h1></li>
+              <li><h1 className='text-xl font-Metropolis-Regular hover:underline transition duration-500 px-8'><a href="#contact">Contact</a></h1></li>
+            </ul>
+          </nav>
+        </section>
+
+        <section className='my-40 flex'>
+            <div className='flex justify-around'>
+                <div className='px-20'>
+                    <h1 className='w-1/3 text-3xl mb-5 tracking-1'>BEAU 3 PIÈCES LUMINEUX ET ENSOLEILLÉ</h1>
+                    <hr className='border-black w-1/6' />
+                    <p className='w-1/2 my-5 text-gray-800'>
+                    Dans le secteur de Gorbella, le Ray, au pied du tramway et des commerces, venez découvrir ce beau 3 pièces en étage élevé. L’appartement de 64 M2 dispose d’une jolie...
+                    </p>
+                    <h2 className='my-5 text-xl text-gray-800'>PRIX :729 €</h2>
+                    <div className='flex'>
+                        <p className='mx-5 border-2 p-2 border-gray-800'>Voir la fiche</p>
+                        <p className='mx-5 border-2 p-2 border-gray-800'>Nous contacter</p>
+                    </div>
+                </div>
+                <div className='w-3/4 flex mx-auto'>
+                    <Image className='object-contain' src={app1} width={250} height={250}/>   
+                </div>
+            </div>
+        </section>
+
+        <section className='my-40 flex'>
+            <div className='flex justify-around'>
+                <div className='px-20'>
+                    <h1 className='w-1/3 text-3xl mb-5'>MAISON RÉNOVÉE</h1>
+                    <hr className='border-black w-1/6' />
+                    <p className='w-1/2 my-5 text-gray-800'>
+                    VENDUE. Située à 15 minutes du centre ville, cette maison édifiée dans un écrin de verdure et baignée de soleil saura vous séduire par son style méditérannéen et...
+                    </p>
+                    <h2 className='my-5 text-xl text-gray-800'>PRIX :1259 €</h2>
+                    <div className='flex'>
+                        <p className='mx-5 border-2 p-2 border-gray-800'>Voir la fiche</p>
+                        <p className='mx-5 border-2 p-2 border-gray-800'>Nous contacter</p>
+                    </div>
+                </div>
+                <div className=' w-3/4 flex mx-auto'>
+                    <Image className='object-contain' src={app2} width={500} height={500}/>   
+                </div>
+            </div>
+        </section>
+
+        <section className='my-40 flex'>
+            <div className='flex justify-around'>
+                <div className='px-20'>
+                    <h1 className='w-1/3 text-3xl mb-5'>DUPLEX PLACE WILSON</h1>
+                    <hr className='border-black w-1/6' />
+                    <p className='w-1/2 my-5 text-gray-800'>
+                    VENDU. PLACE WILSON.DERNIER ÉTAGE. Au cœur de Montluçon, dans un immeuble classé historique, venez découvrir ce magnifique appartement, esprit «villa sur le toit » accompagné de son jardin d’hiver dont...
+                    </p>
+                    <h2 className='my-5 text-xl text-gray-800'>PRIX :1499 €</h2>
+                    <div className='flex'>
+                        <p className='mx-5 border-2 p-2 border-gray-800'>Voir la fiche</p>
+                        <p className='mx-5 border-2 p-2 border-gray-800'>Nous contacter</p>
+                    </div>
+                </div>
+                <div className=' w-3/4 flex mx-auto'>
+                    <Image className='object-contain' src={app3} width={500} height={500}/>   
+                </div>
+            </div>
+        </section>
+
+        <section className='my-40 flex'>
+            <div className='px-20'>
+                <h1 className='w-1/3 text-3xl mb-5'> 3 PIÈCES ÉSPRIT MAISON ET SA VUE PANORAMIQUE</h1>
+                <hr className='border-black w-1/6' />
+                <p className='w-1/2 my-5 text-gray-800'>
+                VENDU Situé dans un écrin de verdure et au calme complet, venez découvrir notre 3 pièces et sa magnifique vue dégagée sur la ville et la mer . L’appartement dispose...
+                </p>
+                <h2 className='my-5 text-xl text-gray-800'>PRIX :750 €</h2>
+                <div className='flex'>
+                    <p className='mx-5 border-2 p-2 border-gray-800'>Voir la fiche</p>
+                    <p className='mx-5 border-2 p-2 border-gray-800'>Nous contacter</p>
+                </div>
+            </div>
+            <div className='w-3/4 flex mx-auto'>
+                <Image className='object-contain' src={app4} width={500} height={500}/>   
+            </div>
+        </section>
+        
+        <footer className='absolute left-0 bg-white text-center text-gray-700 w-full'>
+        <hr className="my-6 border-black" />
+          <div className='my-5'>
+            <a href="#accueil"><Image className='mx-auto' src={design} width={250} height={250}/></a>
+            <p className='text-black'>Horaires : </p>
+            <p>Du lundi au samedi de 9h00 à 19h00</p>
+
+            <div className='flex justify-center text-2xl my-5 gap-5'>
+                <a className='hover:text-black transition duration-500' href="https://www.messenger.com/t/100088349492050/" target='_blank'><BsMessenger/></a>
+                <a className='hover:text-black transition duration-500' href="https://t.me/MelvynHoarau" target='_blank'><BsInstagram/></a>
+                <a className='hover:text-black transition duration-500' href="mailto:melvynhoarau@icloud.com" target='_blank'><BsEnvelopeFill/></a>
+            </div>
+
+            <p>contact@immobilier.com</p>
+            <p>07 67 85 68 37</p>
+            <div>
+                <small>&copy; MELVYN WEBDESIGN. All right reserved.</small>
+            </div>
+          </div>
+        </footer>
+      </main>
+      </div>
+    );
+  };
+  
+  export default ventes;
+  
